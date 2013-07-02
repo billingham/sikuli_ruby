4e181af5d543cfe3fcf801b7e3b0310e7747d0cd
David Billingham <david@Davids-MacBook-Air.local>
2013-07-01 21:22:23 -0400

    Add a capture functionality to the screen

--- lib/sikuli/screen.rb
+++ lib/sikuli/screen.rb
@@ -16,5 +16,26 @@ module Sikuli
     def initialize
       @java_obj = org.sikuli.script::Screen.new()
     end
+
+
+    def capture(*args)
+      @java_obj.capture(*args)
+    end
+
+    def captureDigest(*args)
+      img = capture(*args)
+      #puts v.java_class.declared_instance_methods
+      buffer = img.getImage.getRaster.getDataBuffer
+      #puts 'number of banks',b.getNumBanks
+      data = Array.new
+      j=0
+      while j < buffer.getNumBanks
+        data.push(buffer.getData(j))
+        j+=1
+      end
+
+      'image-capture-data-hash-'+Digest::MD5.hexdigest(data.join)
+    end
+
   end
 end
